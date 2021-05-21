c1baeced864f716929fc007260d364a975a604a4
D3C0RU5 <chpeterle.profissional@gmail.com>
2021-05-21 12:57:35 -0300

    port change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use('/', (req, res) => {
 });
 
 const server = http.createServer(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port);
 
 console.debug('Server inicializado na porta ' + port);
